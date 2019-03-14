52ee6c3a7265ea2b208267f3e6b1353820ba33b7
samitsharma <samit.sharma@accionlabs.com>
2019-03-14 16:19:44 -0700

    change in db

--- createdb.py
+++ createdb.py
@@ -41,7 +41,7 @@ c.execute(""" Create table if not exists tag_detail (
                     tag_id INTEGER NOT NULL REFERENCES tag_head(tag_id),
                     create_time DATETIME,
                     update_time DATETIME,
-                    PRIMARY KEY(article_id,tags),
+                    PRIMARY KEY(article_id,tag_id),
                     FOREIGN KEY (article_id) REFERENCES article(article_id)) """)
 
 conn.commit()
